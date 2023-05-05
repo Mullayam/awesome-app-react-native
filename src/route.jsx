c2a1793332cadc94ee51efc861126db0b53b6c21
Mulayam <50077856+Mullayam@users.noreply.github.com>
2023-05-05 21:30:58 +0530

    initial commit

---
+++ src/route.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
+
+export default function route() {
+  
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.screen name="SignUp" component="" />
+      </Stack.Navigator>
+    </NavigationContainer>
+  
+}
